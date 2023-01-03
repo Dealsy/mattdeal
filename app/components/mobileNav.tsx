42569d23e3768044a81fe4c3e9459b8ea60c0029
Matt Deal <contact@mattdeal.com.au>
2023-01-03 23:01:17 +1100

    updatse links in mobile nav

--- app/components/mobileNav.tsx
+++ app/components/mobileNav.tsx
@@ -89,8 +89,8 @@ export default function MobileNav({ open, setOpen }: MobileNavProps) {
                           >
                             <div className="flex flex-col gap-5 p-10 text-6xl text-gray-900 dark:text-white">
                               <Link to="/">Home</Link>
-                              <Link to="/">Blog</Link>
-                              <Link to="/">About</Link>
+                              <Link to="/posts">Blog</Link>
+                              <Link to="/about">About</Link>
                             </div>
                             <div className="flex flex-row gap-2 ">
                               <ArrowLeftOnRectangleIcon className="absolute left-10 bottom-8 h-14 w-14  text-red-500" />
